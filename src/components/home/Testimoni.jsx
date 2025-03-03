208e8f55f3794dbbaac54f430b85c26073d38b05
Fioreza Radhin Naufal <122506640+Fiorezarn@users.noreply.github.com>
2025-03-03 09:46:49 +0700

    Update Testimoni.jsx

--- src/components/home/Testimoni.jsx
+++ src/components/home/Testimoni.jsx
@@ -7,7 +7,7 @@ import { IoStar } from "react-icons/io5";
 
 const testimonials = [
   {
-    name: "Muhammad Fauzan",
+    name: "Alifia Asri",
     image:
       "https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
     rating: 6,
